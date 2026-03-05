a203a4937b994a2f08caec810cdd8110cd4026ec
Richard Wise <richard.wise@hey.com>
2026-03-05 19:26:28 +0100

    add route for listing image urls

---
+++ app/controllers/blobs_controller.rb
@@ -0,0 +1,5 @@
+class BlobsController < ApplicationController
+  before_action :set_reservation, only: %i[show edit update destroy confirmation]
+  def index
+    @memories = Memory.all
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :reservations
   resources :memories, only: [:index, :new, :create]
+  resources :blobs, only: [:index]
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
